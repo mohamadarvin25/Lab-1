54d53e9c04629ba6cbde6dd57bed6ba980ccb92b
mohamadarvin25 <mohamadarvin84@gmail.com>
2022-09-28 07:00:07 +0700

    upload lab 3

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from wishlist.views import show_wishlist,show_xml,show_json,show_json_by_id
+from wishlist.views import show_wishlist, show_xml, show_json, show_json_by_id, register, login_user, logout_user
 
 app_name = 'wishlist'
 
@@ -8,4 +8,7 @@ urlpatterns = [
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
     path('json/<int:id>', show_json_by_id, name='show_json_by_id'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'), 
 ]
